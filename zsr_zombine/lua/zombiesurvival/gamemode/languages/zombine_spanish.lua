b91a2e5b1baf498d4ff888a3c825d842fe2c250d
Gabipie <32009326+Gabipie@users.noreply.github.com>
2018-04-15 18:07:58 +0100

    Create zombine_spanish.lua
    A spanish translation for the zombine ^^

---
+++ zsr_zombine/lua/zombiesurvival/gamemode/languages/zombine_spanish.lua
@@ -0,0 +1,8 @@
+
+translate.AddLanguage("es", "Spanish")
+
+LANGUAGE.class_zombine	= "Zombine"
+
+LANGUAGE.description_zombine	= "Cuando un headcrab infecta a un Soldado Combine este horrible zombi es creado.\nEste zombi es un peligro para las barricadas y aparece normalmente en oleadas más tarde."
+
+LANGUAGE.controls_zombine	= "> PRIMARIO: Garras\n> SECUNDARIO: Granada"	
